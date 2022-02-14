f34d72b5e63df7c9fb3901daa555d2979922bd60
lina <linah.ahmed.92@gmail.com>
2022-02-14 17:17:35 +0200

    init

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Vue from 'vue';
+import Usecsv from '@/usecsv.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    Usecsv
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <usecsv />
+  </div>
+</template>
---
+++ src/usecsv.vue
@@ -0,0 +1,64 @@
+<script lang="ts">
+import Vue from "vue";
+import useCsvPlugin from "@usecsv/js";
+
+type UserObject = {
+  readonly userId: string;
+};
+interface IUsecsv {
+  importerKey: string;
+  user?: UserObject;
+  metadata: Record<string, string | number> | undefined;
+}
+
+export default /*#__PURE__*/ Vue.extend<{hasSlot:boolean},{onclick:()=>void,hasScopedSlot:()=>boolean},any,IUsecsv>({
+  name: "Usecsv", // vue component name
+  data() {
+    return {
+      hasSlot: !!this.$slots.default,
+    };
+  },
+  methods: {
+    onclick(): void {
+      useCsvPlugin({
+        importerKey: this.importerKey,
+        user: this.importerKey,
+        metadata: this.metadata,
+      });
+    },
+    hasScopedSlot(): boolean {
+      console.log(this.$scopedSlots.default && this.$scopedSlots.default.name);
+      return (
+        (this.$scopedSlots.default && this.$scopedSlots.default.name) ===
+        "normalized"
+      );
+    },
+  },
+});
+</script>
+
+<template>
+  <div class="usecsv">
+    <div v-if="hasScopedSlot()">
+      <slot :onclick="onclick"> open usecsv </slot>
+    </div>
+    <div v-else>
+      <button type="button" id="usecsv-button" @click="onclick">
+        <slot> open usecsv </slot>
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#usecsv-button {
+  background-color: #fff;
+  color: #000;
+  border: 2px solid #000;
+  border-radius: 6px;
+  padding: 10px 15px;
+  text-align: center;
+  font-size: 16px;
+  cursor: pointer;
+}
+</style>
